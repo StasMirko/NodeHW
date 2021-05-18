5575f21d46254c236021830f97177e65a9dcd8cf
Стас <stasmq@gmail.com>
2021-05-18 20:26:40 +0300

    Lecture 3.1

--- L3/controller/user.controller.js
+++ L3/controller/user.controller.js
@@ -1,15 +1,27 @@
+const userService = require('../service/user.service');
+
 module.exports = {
 
     getAllUsers: (req, res) => {
-        res.json('Hello from controller!!!');
+        try {
+            const users = userService.findUsers();
+            res.json(users);    
+        } catch (e) {
+            res.status(400).json(e.message)
+        }
+        
     },
 
     getSingleUser: (req, res) => {
-        res.json(req.params);
+        const {userId} = req.params;
+        const user = userService.findUserById(userId);
+
+        res.json(user);
     },
 
     createUser: (req, res) => {
-        res.json('User is created!!!');
+        userService.createUser(req.body);
+        res.status(201).json('User is created!!!');
     }
 
 };
---
+++ L3/service/user.service.js
@@ -0,0 +1,13 @@
+const DB = require('../dataBase/users');
+
+module.exports = {
+    findUsers: () => {
+        return DB;
+    },
+    findUserById: (userId) => {
+        return DB[userId];
+    },
+    createUser: (userObject) => {
+        DB.push(userObject);
+    }
+};
