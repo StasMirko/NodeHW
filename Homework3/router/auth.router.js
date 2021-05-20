b476d8fbe1c2872f36a95f45a5c28fb6d6e0e739
Стас <stasmq@gmail.com>
2021-05-21 00:36:36 +0300

    HW 3

---
+++ Homework3/router/auth.router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const authController = require('../controller/auth.comtroller');
+const authMiddleware = require('../middleware/auth.middleware');
+
+router.post('/')
+
+module.exports = router;
