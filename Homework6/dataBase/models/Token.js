4380c85c94330a953f2e4193d9b33f863ca29ca1
Стас <stasmq@gmail.com>
2021-08-02 16:16:18 +0300

    HW 6.1

---
+++ Homework6/dataBase/models/Token.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const Token = sequelize.define('Token', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+
+            access_token: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+
+            refresh_token: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+
+            createAt: {
+                type: DataTypes.DATETIME,
+                defaultValue: sequelize.fn('now')
+            }
+        },
+
+        {
+            tableName: 'token',
+            timestamps: false
+        }
+
+    )
+    return Token;
+}
